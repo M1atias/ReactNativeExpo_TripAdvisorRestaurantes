f35c5d3bd1d247b6cc9b0f38ceeccd090aec060f
M1atias <maty.f.molina4a@gmail.com>
2022-05-05 05:47:21 -0300

    add: creación de componentes

---
+++ React/mi-first-app/src/components/HolaMundo.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+export default function HolaMundo() {
+    const mifunction = () =>{
+        console.log('Mi function');
+    };
+    
+    return(
+        <>
+            <h1>
+                Hola Mundo
+            </h1>
+            <h2>
+                Matute
+            </h2>
+        </>
+    );
+}
+
