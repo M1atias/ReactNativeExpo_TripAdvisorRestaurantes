ee07c54fab1e0a1b5fbda930bca8567f545ff960
M1atias <maty.f.molina4a@gmail.com>
2022-05-05 06:31:21 -0300

    add: manejo de props

--- React/mi-first-app/src/App.js
+++ React/mi-first-app/src/App.js
@@ -1,15 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import HolaMundo from './components/HolaMundo';
-import AdiosMundo from './components/AdiosMundo'
+import Saludar from './components/Saludar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <HolaMundo />
-        <AdiosMundo />
+        <Saludar name='Maty' age='22'/>
+        <Saludar name='Matute' age='21'/>
       </header>
     </div>
   );
