3f8a7f055366db2e1e7c0c029aa06d3f8135499a
M1atias <maty.f.molina4a@gmail.com>
2022-05-06 04:31:37 -0300

    add: Se termino con la tercera lección

--- React/mi-first-app/src/App.js
+++ React/mi-first-app/src/App.js
@@ -1,9 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import Saludar from './components/Saludar';
+import {useState, useEffect} from 'react';
 
 function App() {
 
+  const [stateCar, setStateCar] = useState(false);
+  const [contar, setContar] = useState(0);
+
+  useEffect(() => {
+    console.log('Total: ' + contar);
+  }, [contar])
+  
+
+  const encenderApagar = () =>{
+    //setStateCar(!stateCar);
+
+    setStateCar(prevValue => !prevValue);
+    setContar(contar+1);
+  }
   //const userName = 'Maty';
   //const userAge = '26';
 
@@ -13,8 +28,8 @@ function App() {
     color: 'Red'
   }
 
-  const saludarFN = (name) =>{
-    console.log("hola " + name);
+  const saludarFN = (name, edad) =>{
+    console.log(`Hola ${name}, tiene ${edad} años`)
   }
 
 
@@ -22,7 +37,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
+        <h3>El coche esta: {stateCar ? 'Encendido' : ' Apagado'}</h3>
+        <h4>Clicks: {contar} </h4>
+        <button onClick={encenderApagar}>Encender/ Apagar</button>
         <Saludar userInfo={user} saludarFN={saludarFN}/>
       </header>
     </div>
--- React/mi-first-app/src/components/Saludar.js
+++ React/mi-first-app/src/components/Saludar.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 export default function Saludar (props){
+    const {userInfo, saludarFN} = props;
+    const {nombre = 'Anonimo'} = userInfo
 
     return(
         <div>
-           <button onClick={ () => props.saludarFN(props.userInfo.nombre)}>Saludar</button>
+           <button onClick={ () => saludarFN(nombre)}>Saludar</button>
         </div>
     );
 }
