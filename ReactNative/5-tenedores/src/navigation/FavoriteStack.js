97830a05b0e828390d5070763601fa1fe822fd1e
M1atias <maty.f.molina4a@gmail.com>
2022-05-07 10:24:35 -0300

    add: Lección 5

---
+++ ReactNative/5-tenedores/src/navigation/FavoriteStack.js
@@ -0,0 +1,13 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { screen } from "../utils";
+import { FavoritesScreen } from "../screens/FavoritesScreen";
+
+const Stack = createNativeStackNavigator();
+
+export function FavoriteStack() {
+    return(
+        <Stack.Navigator>
+            <Stack.Screen name={screen.favorites.favorites} component= {FavoritesScreen} options= {{title: "Favorites"}}/>
+        </Stack.Navigator>
+    );
+}
