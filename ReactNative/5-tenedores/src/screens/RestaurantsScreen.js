e6ea722a653dd8c090e1351e0da2f762c55e50a0
M1atias <maty.f.molina4a@gmail.com>
2022-05-07 07:35:09 -0300

    add: Se termino la clase 4

---
+++ ReactNative/5-tenedores/src/screens/RestaurantsScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export function RestaurantsScreen() {
+    return(
+        <View>
+            <Text>Estamos en la screen Restaurants</Text>
+        </View>
+    );
+}
