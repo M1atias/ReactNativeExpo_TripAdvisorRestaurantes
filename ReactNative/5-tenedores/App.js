2cd6ef0791333eaae480e0fbeb18fdd391117c5c
M1atias <maty.f.molina4a@gmail.com>
2022-05-07 17:39:04 -0300

    add: Se instalo firebase version @9.6.7

--- ReactNative/5-tenedores/App.js
+++ ReactNative/5-tenedores/App.js
@@ -1,5 +1,6 @@
 import {AppNavigation } from "./src/navigation/AppNavigation";
 import { NavigationContainer } from "@react-navigation/native";
+import { initFirebase } from "./src/utils/";
 
 
 export default function App() {
---
+++ ReactNative/5-tenedores/src/utils/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAUiVRg_CR9tfqBfiBLMuz5MzLjOkNF8Iw",
+  authDomain: "tenedores-8f7d5.firebaseapp.com",
+  projectId: "tenedores-8f7d5",
+  storageBucket: "tenedores-8f7d5.appspot.com",
+  messagingSenderId: "336965512696",
+  appId: "1:336965512696:web:ab9fe213bb3e8a1fe97a0d"
+};
+
+export const initFirebase = initializeApp(firebaseConfig);
