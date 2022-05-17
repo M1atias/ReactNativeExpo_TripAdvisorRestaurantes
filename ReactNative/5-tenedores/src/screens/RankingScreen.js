a0e9d8f6bc3dad15051319f21076f56a3d175e8b
M1atias <maty.f.molina4a@gmail.com>
2022-05-17 04:15:19 -0300

    add: Se termino la sección 14

--- ReactNative/5-tenedores/src/screens/RankingScreen.js
+++ ReactNative/5-tenedores/src/screens/RankingScreen.js
@@ -1,10 +1,36 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React,{useState,useEffect} from 'react'
+import { ScrollView } from 'react-native'
+import { collection,query,orderBy,onSnapshot,limit } from "firebase/firestore";
+import { db } from "../utils";
+import { map } from "lodash";
+import { RestaurantRanking } from "../components/Restaurants";
 
 export function RankingScreen() {
+
+  const [restaurants, setRestaurants] = useState(null);
+
+
+  useEffect(() => {
+    const q = query(
+      collection(db,"restaurants"),
+      orderBy("ratingMedia","desc"),
+      limit(6),
+    );
+
+    onSnapshot(q,(snapshot) =>{
+      setRestaurants(snapshot.docs);
+    });
+  }, []);
+  
   return (
-    <View>
-      <Text>RankingScreen</Text>
-    </View>
+    <ScrollView>
+      {map(restaurants, (restauran, index) => (
+        <RestaurantRanking 
+        key={index}
+        index={index}
+        restaurant={restauran.data()}
+        />
+      ))}
+    </ScrollView>
   )
 }
